8d9bcd0df9b21c1399457d5f5f4474bbc6581536
ComputeJason <jasonchenys1998@gmail.com>
2024-01-15 21:41:24 +0800

    rearrange files

--- FE/template/src/App.js
+++ FE/template/src/App.js
@@ -1,5 +1,5 @@
 import logo from "./logo.svg";
-import "./App.css";
+import "./css/App.css";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Minipostman } from "./components/minipostman/Minipostman";
